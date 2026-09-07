2035e692149476c010a3f5fa9307d8ecb83ac7b9
Saidur Rahman <saidurwd@gmail.com>
2026-09-07 18:22:17 +0600

    Project initialize

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::firstOrCreate(
+            ['username' => 'admin'],
+            [
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('admin'),
+            ]
+        );
+    }
+}
